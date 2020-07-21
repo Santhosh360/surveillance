71942d6984a571ab7e9d68b96fd7c9a220bde7e5
Santhosh360 <40275957+Santhosh360@users.noreply.github.com>
2020-07-21 07:47:17 -0700

    Removed un-necessary imports

--- obj_detect_min.py
+++ obj_detect_min.py
@@ -3,18 +3,8 @@ import pathlib
 import cv2
 
 import numpy as np
-import six
-import six.moves.urllib as urllib
 import sys
-import tarfile
 import tensorflow as tf
-import zipfile
-
-from collections import defaultdict
-from io import StringIO
-from matplotlib import pyplot as plt
-from PIL import Image
-from IPython.display import display
 
 from object_detection.utils import ops as utils_ops
 from object_detection.utils import label_map_util

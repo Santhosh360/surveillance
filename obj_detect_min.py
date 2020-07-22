01758ab46bc84bed6bb6155ca9a0d2b8dfe1a301
Santhosh360 <40275957+Santhosh360@users.noreply.github.com>
2020-07-22 11:39:07 +0530

    Add files via upload

--- obj_detect_min.py
+++ obj_detect_min.py
@@ -26,7 +26,7 @@ PATH_TO_LABELS = 'mscoco_label_map.pbtxt'
 category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
 
 #model_name = 'ssd_mobilenet_v1_coco_2017_11_17'
-model_name = 'faster_rcnn_resnet101_coco_2018_01_28'
+model_name = 'ssd_resnet50_v1_fpn_shared_box_predictor_640x640_coco14_sync_2018_07_03'
 detection_model = load_model(model_name)
 
 def run_inference_for_single_image(model, image):
@@ -53,6 +53,30 @@ def run_inference_for_single_image(model, image):
   output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
  
   return output_dict
+
+def is_person_detected(model,start_frame,vid,fps):
+  person_in_frame = 0
+  for i in range(start_frame,start_frame+fps):
+    vid.set(cv2.CAP_PROP_POS_FRAMES, i)
+    ret, frame = vid.read()
+    if isinstance(frame, type(None)):
+      exit(0)
+    output_dict = run_inference_for_single_image(model, frame)
+    print('FRAME_START')
+    print(vid.get(1))
+    for j in range(0,len(output_dict['detection_classes'])):
+      #print(output_dict['detection_classes'][j],output_dict['detection_scores'][j])
+      if output_dict['detection_classes'][j] == 1 and output_dict['detection_scores'][j] > 0.4:
+        person_in_frame = person_in_frame + 1
+        break
+    print('FRAME_END')
+  print("person_in_frame:",person_in_frame)
+  if int(fps*0.4) < person_in_frame:
+    print("Person detected!!!")
+    return True
+  return False
+
+#def store_video(from_frame,to_frame,)
 
 def show_inference_video(model,video_path):
   # define a video capture object 
@@ -112,4 +136,22 @@ def show_inference_video(model,video_path):
   # Destroy all the windows 
   cv2.destroyAllWindows() 
 
-show_inference_video(detection_model, 'chainSnatching.mkv')
+#show_inference_video(detection_model, 'chainSnatching.mp4')
+
+frme = 0
+video_path = input("Enter video path:")
+vid = cv2.VideoCapture(video_path)
+if (vid.isOpened()== False):
+  print("Error opening video stream or file")
+  exit(0)
+fps = int(vid.get(cv2.CAP_PROP_FPS))
+while True:
+  print("========================================================")
+  res = is_person_detected(detection_model, frme, vid, fps)
+  if res == False:
+    frme = frme + fps
+    print("ff 1sec")
+    continue
+  print("Person detected:",res)
+  frme = frme + (fps*5)
+  print("ff 5secs")

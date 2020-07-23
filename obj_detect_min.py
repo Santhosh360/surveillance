a6c90668592d7f3868e5188c6fbb49fb5c65d567
Santhosh360 <40275957+Santhosh360@users.noreply.github.com>
2020-07-23 15:08:03 +0530

    Add files via upload

--- obj_detect_min.py
+++ obj_detect_min.py
@@ -5,6 +5,7 @@ import cv2
 import numpy as np
 import sys
 import tensorflow as tf
+import math
 
 from object_detection.utils import ops as utils_ops
 from object_detection.utils import label_map_util
@@ -26,7 +27,8 @@ PATH_TO_LABELS = 'mscoco_label_map.pbtxt'
 category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
 
 #model_name = 'ssd_mobilenet_v1_coco_2017_11_17'
-model_name = 'ssd_resnet50_v1_fpn_shared_box_predictor_640x640_coco14_sync_2018_07_03'
+#model_name = 'ssd_resnet50_v1_fpn_shared_box_predictor_640x640_coco14_sync_2018_07_03'
+model_name = 'ssd_mobilenet_v1_coco_2017_11_17'
 detection_model = load_model(model_name)
 
 def run_inference_for_single_image(model, image):
@@ -59,19 +61,23 @@ def is_person_detected(model,start_frame,vid,fps):
   for i in range(start_frame,start_frame+fps):
     vid.set(cv2.CAP_PROP_POS_FRAMES, i)
     ret, frame = vid.read()
-    if isinstance(frame, type(None)):
+    if ret == False:
+      vid.release()
       exit(0)
     output_dict = run_inference_for_single_image(model, frame)
-    print('FRAME_START')
-    print(vid.get(1))
+    #print('<<<-------------------FRAME_START------------------->>>')
+    #print('FRAME_NUMBER:',vid.get(1))
     for j in range(0,len(output_dict['detection_classes'])):
       #print(output_dict['detection_classes'][j],output_dict['detection_scores'][j])
       if output_dict['detection_classes'][j] == 1 and output_dict['detection_scores'][j] > 0.4:
+        print("Person detection score:",output_dict['detection_scores'][j])
         person_in_frame = person_in_frame + 1
         break
-    print('FRAME_END')
+    #print('<<<-------------------FRAME_END------------------->>>')
+    if int(math.ceil(fps*0.1)) < person_in_frame:
+      break
   print("person_in_frame:",person_in_frame)
-  if int(fps*0.4) < person_in_frame:
+  if int(math.ceil(fps*0.1)) < person_in_frame:
     print("Person detected!!!")
     return True
   return False
@@ -81,7 +87,7 @@ def store_video(from_frame,to_frame,vid,out):
   vid.set(1,from_frame)
   for i in range(from_frame,to_frame):
     ret, frame = vid.read()
-    if isinstance(frame, type(None)):
+    if ret == False:
       vid.release()
       out.release()
       exit(0)
@@ -89,19 +95,19 @@ def store_video(from_frame,to_frame,vid,out):
   vid.set(1,prev_frameIndex)
     
 frme = 0
-video_path = input("Enter video path:")
+video_path = sys.argv[1]
 vid = cv2.VideoCapture(video_path)
 if (vid.isOpened()== False):
   print("Error opening video stream or file")
   exit(0)
-dest_video_path = video_path[:-4]+'_res.avi'
+dest_video_path = sys.argv[2]
 print(dest_video_path)
 frame_width = int(vid.get(3))
 frame_height = int(vid.get(4))
 fps = int(vid.get(cv2.CAP_PROP_FPS))
 out = cv2.VideoWriter(dest_video_path, cv2.VideoWriter_fourcc('M','J','P','G'), fps, (frame_width,frame_height))
 while True:
-  print("========================================================")
+  #print("========================================================")
   res = is_person_detected(detection_model, frme, vid, fps)
   if res == False:
     frme = frme + fps
